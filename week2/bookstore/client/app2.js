95e324586b7e7a01a26629690bd2cd08074a75b4
kakato10 <svetimir.ignatov@gmail.com>
2014-07-06 19:08:00 +0300

    Removing some unnecessary files

--- week2/bookstore/client/app2.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-$(document).ready(function(){
-  $(document).on("click", ".read", function(){
-    var targetedModal = $(this).data("target");
-    $(".bs-example-modal-lg").modal("show");
-  });
-});
